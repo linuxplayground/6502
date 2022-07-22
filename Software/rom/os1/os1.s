ffd78b01bacf7f1755c7fabfea0eaad780f41794
David Latham <kiwilathams@gmail.com>
2022-07-22 19:50:12 +1200

    removed keyboard handling

--- Software/rom/os1/os1.s
+++ Software/rom/os1/os1.s
@@ -3,7 +3,6 @@
       .include "lcd.inc"
       .include "core.inc"
       .include "acia.inc"
-      .include "keyboard.inc"
       .include "syscalls.inc"
 
       .import _run_shell
@@ -32,19 +31,6 @@ init:
       jsr _system_init
       ; Display hello message
       write_lcd #os1_version
-      ; Display keyboard status
-      ldx #$00
-      ldy #$01
-      jsr lcd_set_position
-      lda #1
-      jsr _delay_sec
-      jsr _keyboard_is_connected
-      cmp #(KEYBOARD_NOT_CONNECTED)
-      beq @no_keyboard
-      write_lcd #keyboard_connected
-      bra @wait_for_1s
-@no_keyboard:
-      write_lcd #keyboard_disconnected
 @wait_for_1s:
       lda #01
       jsr _delay_sec
@@ -54,14 +40,9 @@ init:
 @wait_for_acia_input:
       jsr _acia_is_data_available
       cmp #(ACIA_NO_DATA_AVAILABLE)
-      beq @check_keyboard
+      beq @wait_for_acia_input
       jsr _acia_read_byte
       bra @run_shell
-@check_keyboard:
-      jsr _keyboard_is_data_available
-      cmp #(KEYBOARD_NO_DATA_AVAILABLE)
-      beq @wait_for_acia_input
-      jsr _keyboard_read_char
 @run_shell:
       jsr _lcd_clear
       write_lcd #shell_connected
@@ -74,10 +55,6 @@ init:
 
 os1_version:
       .asciiz "OS/1 version 0.3.0C"
-keyboard_disconnected:
-      .asciiz "No keyboard"
-keyboard_connected:
-      .asciiz "Keyboard connected"
 instruction:
       .asciiz "Connect serial port (19200 N8S1 CTS/RTS) and press any key in terminal window"
 shell_connected:
